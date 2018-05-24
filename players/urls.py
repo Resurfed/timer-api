a154f6f3df7634ace3d4c850a074b93a8e47665e
Josh Lovins <12jdlovins@gmail.com>
2018-05-24 17:43:16 -0400

    stuff

--- players/urls.py
+++ players/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from .views import PlayerList, PlayerInfoByID ,IPList
 
-urlpatterns = []
+urlpatterns = [
+    path('', PlayerList.as_view()),
+    path('<int:pk>', PlayerInfoByID.as_view()),
+    path('<int:pk>/ips', IPList.as_view() )
+]
 urlpatterns = format_suffix_patterns(urlpatterns)
